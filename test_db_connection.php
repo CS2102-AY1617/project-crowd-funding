8c7f1fee98e568f5a0f0e7af988cb89e7dcae021
SongYikun <syk950527@gmail.com>
2016-09-08 16:54:30 +0800

    db connection successful. configuring wordpress

---
+++ test_db_connection.php
@@ -0,0 +1,7 @@
+<?php
+phpinfo();
+$connection_string = "host=elmer-01.db.elephantsql.com dbname=chnqvfdr user=chnqvfdr password=EaPfDP-uURFmqH1FXVMabcrbPRToeioD";
+$conn = pg_connect($connection_string);
+
+
+ ?>
