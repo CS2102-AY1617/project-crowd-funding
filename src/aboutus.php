7030a3d68c452d1383a444b97227afed21b01dbb
SongYikun <syk950527@gmail.com>
2016-09-22 02:48:40 +0800

    About us page front end

---
+++ src/aboutus.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="SHIELD - Free Bootstrap 3 Theme">
+        <meta name="author" content="Carlos Alvarez - Alvarez.is - blacktie.co">
+        <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+        <title> CS2102</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="assets/css/main.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/icomoon.css">
+        <link href="assets/css/animate-custom.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+
+        <script src="assets/js/jquery.min.js"></script>
+        <script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="assets/js/html5shiv.js"></script>
+        <script src="assets/js/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body data-spy="scroll" data-offset="0" data-target="#navbar-main">
+    <?php
+    include "header.php";
+    ?>
+
+    <!-- ==== SECTION DIVIDER3 -->
+    <section class="section-divider textdivider divider3">
+        <div class="container">
+            <h1>Project Name</h1>
+            <hr>
+            <p>From the purely practical to the richly philosophical, design is the solution to a host of challenges.</p>
+        </div><!-- container -->
+    </section><!-- section -->
+
+    <!-- ==== BLOG ==== -->
+    <div class="container" id="blog" name="blog">
+        <br>
+        <div class="row">
+            <br>
+            <h1 class="centered">MEET THE TEAM</h1>
+            <hr>
+            <br>
+            <br>
+        </div><!-- /row -->
+
+        <div class="row">
+            <div class="col-lg-12 blog-bg" >
+                <div class="col-lg-4 centered">
+                    <br>
+                    <p><img class="img img-circle" src="assets/img/team/team04.jpg" width="60px" height="60px"></p>
+                    <h4>Jaye Smith</h4>
+                    <h5>Published Aug 30.</h5>
+                </div>
+                <div class="col-lg-8 blog-content">
+                    <h2>We Define Success</h2>
+                    <p>Armed with insight, we embark on designing the right brand experience that engages the audience. It encompasses both the strategic direction and creative execution that solves a business problem and brings the brand to life.</p>
+                    <p>In the create phase, the big idea is unleashed to the world through different media touchpoints. This is when we watch the audience fall in love all over again with our client’s brand.</p>
+                    <p><a href="#" class="icon icon-link"> Read More</a></p>
+                    <br>
+                </div>
+            </div><!-- /col -->
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-lg-12 blog-bg" >
+                <div class="col-lg-4 centered">
+                    <br>
+                    <p><img class="img img-circle" src="assets/img/team/team04.jpg" width="60px" height="60px"></p>
+                    <h4>Jaye Smith</h4>
+                    <h5>Published Aug 30.</h5>
+                </div>
+                <div class="col-lg-8 blog-content">
+                    <h2>We Define Success</h2>
+                    <p>Armed with insight, we embark on designing the right brand experience that engages the audience. It encompasses both the strategic direction and creative execution that solves a business problem and brings the brand to life.</p>
+                    <p>In the create phase, the big idea is unleashed to the world through different media touchpoints. This is when we watch the audience fall in love all over again with our client’s brand.</p>
+                    <p><a href="#" class="icon icon-link"> Read More</a></p>
+                    <br>
+                </div>
+            </div><!-- /col -->
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-lg-12 blog-bg" >
+                <div class="col-lg-4 centered">
+                    <br>
+                    <p><img class="img img-circle" src="assets/img/team/team04.jpg" width="60px" height="60px"></p>
+                    <h4>Jaye Smith</h4>
+                    <h5>Published Aug 30.</h5>
+                </div>
+                <div class="col-lg-8 blog-content">
+                    <h2>We Define Success</h2>
+                    <p>Armed with insight, we embark on designing the right brand experience that engages the audience. It encompasses both the strategic direction and creative execution that solves a business problem and brings the brand to life.</p>
+                    <p>In the create phase, the big idea is unleashed to the world through different media touchpoints. This is when we watch the audience fall in love all over again with our client’s brand.</p>
+                    <p><a href="#" class="icon icon-link"> Read More</a></p>
+                    <br>
+                </div>
+            </div><!-- /col -->
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-lg-12 blog-bg" >
+                <div class="col-lg-4 centered">
+                    <br>
+                    <p><img class="img img-circle" src="assets/img/team/team04.jpg" width="60px" height="60px"></p>
+                    <h4>Jaye Smith</h4>
+                    <h5>Published Aug 30.</h5>
+                </div>
+                <div class="col-lg-8 blog-content">
+                    <h2>We Define Success</h2>
+                    <p>Armed with insight, we embark on designing the right brand experience that engages the audience. It encompasses both the strategic direction and creative execution that solves a business problem and brings the brand to life.</p>
+                    <p>In the create phase, the big idea is unleashed to the world through different media touchpoints. This is when we watch the audience fall in love all over again with our client’s brand.</p>
+                    <p><a href="#" class="icon icon-link"> Read More</a></p>
+                    <br>
+                </div>
+            </div><!-- /col -->
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-lg-12 blog-bg" >
+                <div class="col-lg-4 centered">
+                    <br>
+                    <p><img class="img img-circle" src="assets/img/team/team04.jpg" width="60px" height="60px"></p>
+                    <h4>Jaye Smith</h4>
+                    <h5>Published Aug 30.</h5>
+                </div>
+                <div class="col-lg-8 blog-content">
+                    <h2>We Define Success</h2>
+                    <p>Armed with insight, we embark on designing the right brand experience that engages the audience. It encompasses both the strategic direction and creative execution that solves a business problem and brings the brand to life.</p>
+                    <p>In the create phase, the big idea is unleashed to the world through different media touchpoints. This is when we watch the audience fall in love all over again with our client’s brand.</p>
+                    <p><a href="#" class="icon icon-link"> Read More</a></p>
+                    <br>
+                </div>
+            </div><!-- /col -->
+        </div>
+        <br>
+
+        <br>
+        <br>
+    </div><!-- /container -->
+
+    <?php
+    include "footer.php";
+    ?>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/retina.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="assets/js/smoothscroll.js"></script>
+    <script type="text/javascript" src="assets/js/jquery-func.js"></script>
+    </body>
+</html>
--- src/header.php
+++ src/header.php
@@ -5,13 +5,13 @@
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
           <span class="icon icon-shield" style="font-size:30px; color:#3498db;"></span>
         </button>
-        <a class="navbar-brand page-scroll" href="#first"></i> Project Name</a>
+        <a class="navbar-brand page-scroll" href="landingpage.php"></i> Project Name</a>
       </div>
       <div class="navbar-collapse collapse">
         <ul class="nav navbar-nav">
-          <li><a href="landingpage.php" class="smoothScroll">Discover</a></li>
-          <li> <a href="discover.php" class="smoothScroll">Start A Project</a></li>
-          <li> <a href="#services" class="smoothScroll"> About Us</a></li>
+          <li><a href="discover.php" class="smoothScroll">Discover</a></li>
+          <li> <a href="#" class="smoothScroll">Start A Project</a></li>
+          <li> <a href="aboutus.php" class="smoothScroll"> About Us</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
             <li>
