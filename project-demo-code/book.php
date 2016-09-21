6d5e008bf759076e61c024dc9c976108c27e4c92
SongYikun <syk950527@gmail.com>
2016-09-21 23:15:58 +0800

    .

--- project-demo-code/book.php
+++ project-demo-code/book.php
@@ -1,17 +1,4 @@
-<?php
 
-$servername = "sql6.freesqldatabase.com";
-$username = "sql6135912";
-$password = "6Umu2bknNB";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
 
 
 
