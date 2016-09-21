9c9798f6079437d971bbd02f67b7a22cdc64b264
SongYikun <syk950527@gmail.com>
2016-09-22 02:38:22 +0800

    discover page front end skeleton done

---
+++ src/discover.php
@@ -0,0 +1,369 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="SHIELD - Free Bootstrap 3 Theme">
+	<meta name="author" content="Carlos Alvarez - Alvarez.is - blacktie.co">
+	<link rel="shortcut icon" href="assets/ico/favicon.png">
+
+	<title> CS2102</title>
+
+	<!-- Bootstrap core CSS -->
+	<link href="assets/css/bootstrap.css" rel="stylesheet">
+
+	<!-- Custom styles for this template -->
+	<link href="assets/css/main.css" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/icomoon.css">
+	<link href="assets/css/animate-custom.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+
+	<script src="assets/js/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="assets/js/html5shiv.js"></script>
+	  <script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+	</head>
+	<body data-spy="scroll" data-offset="0" data-target="#navbar-main">
+		<?php
+		  include "header.php";
+		?>
+
+		<!-- ==== SECTION DIVIDER3 -->
+		<section class="section-divider textdivider divider3">
+			<div class="container">
+				<h1>DESIGN SOLVE PROBLEMS</h1>
+				<hr>
+				<p>From the purely practical to the richly philosophical, design is the solution to a host of challenges.</p>
+			</div><!-- container -->
+		</section><!-- section -->
+
+		<!-- ==== PORTFOLIO ==== -->
+		<div class="container" id="portfolio" name="portfolio">
+			<br>
+			<div class="row">
+				<br>
+				<h1 class="centered">WE CREATE COOL STUFF</h1>
+				<hr>
+				<br>
+				<br>
+			</div><!-- /row -->
+			<div class="container">
+				<div class="row">
+
+				<!-- PORTFOLIO IMAGE 1 -->
+				<div class="col-md-3 ">
+			    	<div class="grid mask">
+						<figure>
+							<img class="img-responsive" src="assets/img/portfolio/folio01.jpg" alt="">
+							<figcaption>
+								<h5>DASHBOARD</h5>
+								<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+							</figcaption><!-- /figcaption -->
+						</figure><!-- /figure -->
+			    	</div><!-- /grid-mask -->
+				</div><!-- /col -->
+
+				<!-- PORTFOLIO IMAGE 2 -->
+				<div class="col-md-3">
+			    	<div class="grid mask">
+						<figure>
+							<img class="img-responsive" src="assets/img/portfolio/folio02.jpg" alt="">
+							<figcaption>
+								<h5>UI DESIGN</h5>
+								<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+							</figcaption><!-- /figcaption -->
+						</figure><!-- /figure -->
+			    	</div><!-- /grid-mask -->
+				</div><!-- /col -->
+
+				<!-- PORTFOLIO IMAGE 3 -->
+				<div class="col-md-3">
+			    	<div class="grid mask">
+						<figure>
+							<img class="img-responsive" src="assets/img/portfolio/folio03.jpg" alt="">
+							<figcaption>
+								<h5>ANDROID PAGE</h5>
+								<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+							</figcaption><!-- /figcaption -->
+						</figure><!-- /figure -->
+			    	</div><!-- /grid-mask -->
+				</div><!-- /col -->
+        <div class="col-md-3">
+			    	<div class="grid mask">
+						<figure>
+							<img class="img-responsive" src="assets/img/portfolio/folio03.jpg" alt="">
+							<figcaption>
+								<h5>ANDROID PAGE</h5>
+								<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+							</figcaption><!-- /figcaption -->
+						</figure><!-- /figure -->
+			    	</div><!-- /grid-mask -->
+				</div><!-- /col -->
+			</div><!-- /row -->
+				<br>
+ 				<div class="row">
+				<div class="col-md-3 ">
+			    	<div class="grid mask">
+						<figure>
+							<img class="img-responsive" src="assets/img/portfolio/folio04.jpg" alt="">
+							<figcaption>
+								<h5>PROFILE</h5>
+								<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+							</figcaption><!-- /figcaption -->
+						</figure><!-- /figure -->
+			    	</div><!-- /grid-mask -->
+				</div><!-- /col -->
+
+				<!-- PORTFOLIO IMAGE 5 -->
+				<div class="col-md-3">
+			    	<div class="grid mask">
+						<figure>
+							<img class="img-responsive" src="assets/img/portfolio/folio05.jpg" alt="">
+							<figcaption>
+								<h5>TWITTER STATUS</h5>
+								<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+							</figcaption><!-- /figcaption -->
+						</figure><!-- /figure -->
+			    	</div><!-- /grid-mask -->
+				</div><!-- /col -->
+
+				<!-- PORTFOLIO IMAGE 6 -->
+				<div class="col-md-3">
+			    	<div class="grid mask">
+						<figure>
+							<img class="img-responsive" src="assets/img/portfolio/folio06.jpg" alt="">
+							<figcaption>
+								<h5>PHONE MOCKUP</h5>
+								<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+							</figcaption><!-- /figcaption -->
+						</figure><!-- /figure -->
+			    	</div><!-- /grid-mask -->
+				</div><!-- /col -->
+        <div class="col-md-3">
+			    	<div class="grid mask">
+						<figure>
+							<img class="img-responsive" src="assets/img/portfolio/folio06.jpg" alt="">
+							<figcaption>
+								<h5>PHONE MOCKUP</h5>
+								<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+							</figcaption><!-- /figcaption -->
+						</figure><!-- /figure -->
+			    	</div><!-- /grid-mask -->
+				</div><!-- /col -->
+			</div><!-- /row -->
+				<br>
+				<div class="row">
+					<div class="col-md-3 ">
+						<div class="grid mask">
+							<figure>
+								<img class="img-responsive" src="assets/img/portfolio/folio04.jpg" alt="">
+								<figcaption>
+									<h5>PROFILE</h5>
+									<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+								</figcaption><!-- /figcaption -->
+							</figure><!-- /figure -->
+						</div><!-- /grid-mask -->
+					</div><!-- /col -->
+
+					<!-- PORTFOLIO IMAGE 5 -->
+					<div class="col-md-3">
+						<div class="grid mask">
+							<figure>
+								<img class="img-responsive" src="assets/img/portfolio/folio05.jpg" alt="">
+								<figcaption>
+									<h5>TWITTER STATUS</h5>
+									<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+								</figcaption><!-- /figcaption -->
+							</figure><!-- /figure -->
+						</div><!-- /grid-mask -->
+					</div><!-- /col -->
+
+					<!-- PORTFOLIO IMAGE 6 -->
+					<div class="col-md-3">
+						<div class="grid mask">
+							<figure>
+								<img class="img-responsive" src="assets/img/portfolio/folio06.jpg" alt="">
+								<figcaption>
+									<h5>PHONE MOCKUP</h5>
+									<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+								</figcaption><!-- /figcaption -->
+							</figure><!-- /figure -->
+						</div><!-- /grid-mask -->
+					</div><!-- /col -->
+					<div class="col-md-3">
+						<div class="grid mask">
+							<figure>
+								<img class="img-responsive" src="assets/img/portfolio/folio06.jpg" alt="">
+								<figcaption>
+									<h5>PHONE MOCKUP</h5>
+									<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+								</figcaption><!-- /figcaption -->
+							</figure><!-- /figure -->
+						</div><!-- /grid-mask -->
+					</div><!-- /col -->
+				</div>
+				<br>
+				<div class="row">
+					<div class="col-md-3 ">
+						<div class="grid mask">
+							<figure>
+								<img class="img-responsive" src="assets/img/portfolio/folio04.jpg" alt="">
+								<figcaption>
+									<h5>PROFILE</h5>
+									<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+								</figcaption><!-- /figcaption -->
+							</figure><!-- /figure -->
+						</div><!-- /grid-mask -->
+					</div><!-- /col -->
+
+					<!-- PORTFOLIO IMAGE 5 -->
+					<div class="col-md-3">
+						<div class="grid mask">
+							<figure>
+								<img class="img-responsive" src="assets/img/portfolio/folio05.jpg" alt="">
+								<figcaption>
+									<h5>TWITTER STATUS</h5>
+									<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+								</figcaption><!-- /figcaption -->
+							</figure><!-- /figure -->
+						</div><!-- /grid-mask -->
+					</div><!-- /col -->
+
+					<!-- PORTFOLIO IMAGE 6 -->
+					<div class="col-md-3">
+						<div class="grid mask">
+							<figure>
+								<img class="img-responsive" src="assets/img/portfolio/folio06.jpg" alt="">
+								<figcaption>
+									<h5>PHONE MOCKUP</h5>
+									<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+								</figcaption><!-- /figcaption -->
+							</figure><!-- /figure -->
+						</div><!-- /grid-mask -->
+					</div><!-- /col -->
+					<div class="col-md-3">
+						<div class="grid mask">
+							<figure>
+								<img class="img-responsive" src="assets/img/portfolio/folio06.jpg" alt="">
+								<figcaption>
+									<h5>PHONE MOCKUP</h5>
+									<a data-toggle="modal" href="#myModal" class="btn btn-primary btn-lg">Take a Look</a>
+								</figcaption><!-- /figcaption -->
+							</figure><!-- /figure -->
+						</div><!-- /grid-mask -->
+					</div><!-- /col -->
+				</div>
+			</div>
+	   </div><!-- /container -->
+
+
+		<!-- ==== BLOG ==== -->
+		<div class="container" id="blog" name="blog">
+			<br>
+			<div class="row">
+				<br>
+				<h1 class="centered">WE ARE STORYTELLERS</h1>
+				<hr>
+				<br>
+				<br>
+			</div><!-- /row -->
+
+			<div class="row">
+				<div class="col-lg-6 blog-bg">
+					<div class="col-lg-4 centered">
+					<br>
+						<p><img class="img img-circle" src="assets/img/team/team04.jpg" width="60px" height="60px"></p>
+						<h4>Jaye Smith</h4>
+						<h5>Published Aug 30.</h5>
+					</div>
+					<div class="col-lg-8 blog-content">
+						<h2>We Define Success</h2>
+						<p>Armed with insight, we embark on designing the right brand experience that engages the audience. It encompasses both the strategic direction and creative execution that solves a business problem and brings the brand to life.</p>
+						<p>In the create phase, the big idea is unleashed to the world through different media touchpoints. This is when we watch the audience fall in love all over again with our client’s brand.</p>
+						<p><a href="#" class="icon icon-link"> Read More</a></p>
+						<br>
+					</div>
+				</div><!-- /col -->
+
+				<div class="col-lg-6 blog-bg">
+					<div class="col-lg-4 centered">
+					<br>
+						<p><img class="img img-circle" src="assets/img/team/team03.jpg" width="60px" height="60px"></p>
+						<h4>Michele Lampa</h4>
+						<h5>Published Aug 28.</h5>
+					</div>
+					<div class="col-lg-8 blog-content">
+						<h2>A Beautiful Story</h2>
+						<p>Armed with insight, we embark on designing the right brand experience that engages the audience. It encompasses both the strategic direction and creative execution that solves a business problem and brings the brand to life.</p>
+						<p>In the create phase, the big idea is unleashed to the world through different media touchpoints. This is when we watch the audience fall in love all over again with our client’s brand.</p>
+						<p><a href="#" class="icon icon-link"> Read More</a></p>
+						<br>
+					</div>
+				</div><!-- /col -->
+			</div><!-- /row -->
+			<br>
+			<br>
+		</div><!-- /container -->
+
+
+
+
+		<div id="footerwrap" style="color:white">
+			<div class="container">
+				<div class="col-lg-4">
+					<h3 style="color:white">Contact Information</h3>
+					<p><span class="icon icon-home"></span> Some Address 987, NY<br/>
+						<span class="icon icon-phone"></span> +34 9884 4893 <br/>
+						<span class="icon icon-mobile"></span> +34 59855 9853 <br/>
+						<span class="icon icon-envelop"></span> <a href="#"> agency@blacktie.co</a> <br/>
+						<span class="icon icon-twitter"></span> <a href="#"> @blacktie_co </a> <br/>
+						<span class="icon icon-facebook"></span> <a href="#"> BlackTie Agency </a> <br/>
+					</p>
+				</div><!-- col -->
+				<div class="col-lg-4">
+					<h3 style="color:white">Newsletter</h3>
+					<p>Register to our newsletter and be updated with the latests information regarding our services, offers and much more.</p>
+					<p>
+					<form class="form-horizontal" role="form">
+						<div class="form-group">
+							<label for="inputEmail1" class="col-lg-4 control-label"></label>
+						</div>
+						<div class="form-group">
+							<label for="text1" class="col-lg-4 control-label"></label>
+							<div class="col-lg-10">
+								<input type="text" class="form-control" id="text1" placeholder="Your Name">
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="col-lg-10">
+								<button type="submit" class="btn btn-success">Subscribe</button>
+							</div>
+						</div>
+					</form><!-- form -->
+					</p>
+				</div><!-- col -->
+				<div class="col-lg-4">
+					<h3 style="color:white">Support Us</h3>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+				</div><!-- col -->
+			</div>
+			<h4>Created by <a href="http://blacktie.co">BlackTie.co</a> - Copyright 2014</h4>
+		</div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/retina.js"></script>
+	<script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="assets/js/smoothscroll.js"></script>
+	<script type="text/javascript" src="assets/js/jquery-func.js"></script>
+  </body>
+</html>
