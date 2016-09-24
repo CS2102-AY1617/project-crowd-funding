7165a1b48f072bcfd74232487ffb1e31005e1fcd
SongYikun <syk950527@gmail.com>
2016-09-25 00:16:37 +0800

    minimum front end skeleton done. moving on to backend and database

--- src/startproject.php
+++ src/startproject.php
@@ -77,7 +77,7 @@ include "header.php";
                                             <h2>We Define Success</h2>
                                             <p>Armed with insight, we embark on designing the right brand experience that engages the audience. It encompasses both the strategic direction and creative execution that solves a business problem and brings the brand to life.</p>
                                             <p>In the create phase, the big idea is unleashed to the world through different media touchpoints. This is when we watch the audience fall in love all over again with our client’s brand.</p>
-                                            <p><a href="#" class="icon icon-link"> Read More</a></p>
+                                            <p><a href="project.php" class="icon icon-link"> Read More</a></p>
                                             <br>
                                         </div>
                                     </div><!-- /col -->
